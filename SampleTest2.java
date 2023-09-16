222d8afbf083a31cd5482bf8cc8958f7832c9806
yoshmi <yossshhhi@gmail.com>
2023-09-16 20:29:55 +0900

    first commit

---
+++ SampleTest2.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class SampleTest3 {
+
+    @Nested
+    class getColor {
+        @Test
+        void success() {
+            assertEquals("赤", Sample.getColor(1));
+            assertEquals("緑", Sample.getColor(2));
+            assertEquals("青", Sample.getColor(3));
+        }
+        
+        @Test
+        @DisplayName("引数エラー")
+        void fail() {
+        	assertThrows(IllegalArgumentException.class, ()->{Sample.getColor(99);});
+        }
+    }
+    
+}
+    
